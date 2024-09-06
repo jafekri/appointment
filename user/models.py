97f08e4cc53110c4fd74df8eae09d13bd112dd23
Setare <setare.behzadi@gmail.com>
2024-09-06 12:32:41 +0330

    initialize and user

---
+++ user/models.py
@@ -0,0 +1,135 @@
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+# Create your models here.
+class User(AbstractUser):
+    """
+       username and password 
+       inherited from AbstractUser
+    """
+    first_name = models.CharField(max_length=150, blank=True, null=True)
+    last_name = models.CharField(max_length=150, blank=True, null=True)
+    balance = models.BigIntegerField(default=0)  
+    otp_code = models.IntegerField(blank=True, null=True)  
+    phone = models.CharField(
+        max_length=11, 
+        unique=True, 
+        validators=[
+            RegexValidator(regex=r"^09[0-9]{9}$",
+                           message="Phone number is not correct!")
+        ]
+    )
+    
+    
+    """
+        To avoid interference with auth.User.groups
+    """
+    groups = models.ManyToManyField(
+       'auth.Group',
+        related_name='user_groups',
+        blank=True,
+    )
+    
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='user_permissions',
+        blank=True,
+    )
+    
+    
+    def get_role(self):
+        if hasattr(self, "doctorprofile") and self.doctorprofile:
+            return "doctor"
+        elif hasattr(self, "patientprofile") and self.patientprofile:
+            return "patient"
+        else:
+            return "admin"
+    
+
+    def __str__(self):
+        return self.username
+    
+
+
+class Specialization(models.Model):
+    name = models.CharField(_("Specialization Name"),
+                            max_length=100)
+    slug = models.SlugField(_("Slug"), 
+                            unique=True, 
+                            blank=True)
+
+
+    def __str__(self):
+        return self.name
+    
+
+
+class DoctorProfile(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        verbose_name=_("User"),
+    )
+    specializationID = models.ForeignKey(Specialization,
+                                       on_delete=models.CASCADE, 
+                                       verbose_name=_("Specialization"))
+    experience = models.PositiveIntegerField(_("Years of Experience"))
+    visit_fee = models.DecimalField(_("Consultation Fee"),
+                                           max_digits=10,
+                                           decimal_places=2)
+    average_rating = models.DecimalField(_("Rating"), 
+                                 max_digits=3, 
+                                 decimal_places=2, 
+                                 default=0)
+
+
+    def __str__(self):
+        return f"Doctor Profile of {self.user.username}"
+    
+    
+
+class PatientProfile(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        verbose_name=_("User"),
+    )
+    
+    
+    def __str__(self):
+        return f"Patient Profile of {self.user.username}"
+    
+    
+    
+class DoctorUser(User):
+    class Meta:
+        proxy = True
+
+    class DoctorManager(models.Manager):
+        def get_queryset(self):
+            return super().get_queryset().filter(doctorprofile__isnull=False)
+
+    objects = DoctorManager()
+
+    @property
+    def extra(self):
+        return self.doctorprofile
+
+
+
+class PatientUser(User):
+    class Meta:
+        proxy = True
+
+    class PatientManager(models.Manager):
+        def get_queryset(self):
+            return super().get_queryset().filter(patientprofile__isnull=False)
+
+    objects = PatientManager()
+
+    @property
+    def extra(self):
+        return self.patientprofile
