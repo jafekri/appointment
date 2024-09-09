8ff76899fa8749d0d9cbe559339485d4b0007dd2
mnateghi1 <mohammadaminnateghi2003@gmail.com>
2024-09-10 01:15:51 +0330

    initialize reservation and appointment_time models in reservation/models.py

--- reservation/models.py
+++ reservation/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from user.models import *
+from doctor.models import *
+from datetime import date
+from django.utils import timezone
 
 # Create your models here.
+class AppointmentTime(models.Model):
+    class Status(models.TextChoices):
+        EMPTY = '0', 'EMPTY'
+        COMPLETION = '1', 'COMPLETION'
+
+    doctor_id = models.ForeignKey(Doctor, on_delete=models.CASCADE, related_name='doctor_reservations')
+    start_time = models.DateTimeField(default=timezone.now)
+    end_time = models.DateTimeField(default=timezone.now)
+    appointment_date = models.DateField(default=date.today)
+    status = models.CharField(max_length=1, choices=Status.choices, default=Status.EMPTY)
+
+
+class Reservation(models.Model):
+    class PaymentStatus(models.TextChoices):
+        DONE = 'DN', 'DONE'
+        PENDENT = 'PD', 'PENDENT'
+        CANCELED = 'CN', 'CANCELED'
+
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_reservations')
+    doctor_id = models.ForeignKey(Doctor, on_delete=models.CASCADE, related_name='doctor_reservations')
+    appointment_id = models.OneToOneField(AppointmentTime, on_delete=models.CASCADE)
+    visit_fee = models.PositiveIntegerField(default=0)
+    payment_status = models.CharField(max_length=2, choices=PaymentStatus.choices, default=PaymentStatus.PENDENT)
+
