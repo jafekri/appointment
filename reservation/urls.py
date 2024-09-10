5a3594ebb69db747c9226b1a2366d8c8820a12e1
Setare <setare.behzadi@gmail.com>
2024-09-10 13:29:20 +0330

    revert reservation first commit

--- reservation/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from reservation.views import ReservationView
-
-app_name = 'reservation'
-
-urlpatterns = [
-    path('reservation/', ReservationView.as_view(), name='reservation'),
-]
