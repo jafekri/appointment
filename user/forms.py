5d455176f918789874c6a11bfcade5c9d905d893
Setare <setare.behzadi@gmail.com>
2024-09-10 23:04:51 +0330

    refactor sign up and add otp_code and roles

--- user/forms.py
+++ user/forms.py
@@ -1,8 +1,19 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.core.exceptions import ValidationError
 from .models import User
 
 class CustomUserCreationForm(UserCreationForm):
+    ROLE_CHOICES = [
+        ('doctor', 'Doctor'),
+        ('patient', 'Patient'),
+        ('admin', 'Admin'),
+    ]
+
+    password1 = forms.CharField(label='password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='confirm password', widget=forms.PasswordInput)
+    role = forms.ChoiceField(choices=ROLE_CHOICES, required=True)
+    specialization = forms.CharField(required=False)
     class Meta:
         model = User
         fields = ('first_name',
@@ -10,10 +21,29 @@ class CustomUserCreationForm(UserCreationForm):
                   'username',
                   'phone',
                   'password1',
-                  'password2',)
+                  'password2',
+                   'role',
+                  'specialization')
+
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            if self.data.get('role') == 'doctor':
+                self.fields['specialization'].required = True  # فیلد تخصص را اجباری می‌کنیم
+            else:
+                self.fields['specialization'].widget = forms.HiddenInput()  # مخفی کردن فیلد تخصص برای نقش‌های دیگر
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password1'] and cd['password2'] and cd['password1'] != cd['password2']:
+            raise ValidationError("Passwords don't match")
+        else:
+            return cd['password2']
 
 class CustomAuthenticationForm(AuthenticationForm):
     class Meta:
         model = User
         fields = ('username',
                   'password',)
+
+
+class VerifyCodeForm(forms.Form):
+    code = forms.IntegerField()
--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import SignUpView, CustomLoginView, LogoutView
+from .views import SignUpView, CustomLoginView, LogoutView, VerifyCodeView
 
 app_name = 'user'
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
+    path('verify/', VerifyCodeView.as_view(), name='verify_code'),
     path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
 ]
