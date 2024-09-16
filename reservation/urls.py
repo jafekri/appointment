87869e61da92dcebb4da816b2b777081ad906564
setare behzadi <setare.behzadi@gmail.com>
2024-09-16 09:29:15 +0330

    Feature/reservation (#38)
    * appointment for a doctor
    
    * backend and fromt for appointment and reservation

---
+++ reservation/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from reservation.views import ReservationCreateView
+
+app_name = 'reservation'
+
+urlpatterns = [
+    path('<int:pk>/', ReservationCreateView.as_view(), name='make_reservation'),
+
+]
