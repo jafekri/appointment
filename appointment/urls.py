0734235a42d6c0721f4f50b96442367c06620b4a
Mojtaba <mghasemi13786@gmail.com>
2024-10-05 19:57:02 +0330

    refactor urls

--- appointment/urls.py
+++ appointment/urls.py
@@ -1,37 +1,23 @@
-"""
-URL configuration for appointment project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth.views import LoginView
+
 from user.views import SignUpView, VerifyCodeView
 from user.forms import AuthenticationForm
-from django.contrib.auth.views import LoginView
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    
     # auth urls
     path("auth/login/", LoginView.as_view(form_class=AuthenticationForm), name="login"),
     path("auth/", include("django.contrib.auth.urls")),
     path("auth/register/", SignUpView.as_view(), name="signup"),
     path("auth/register/doctor/", SignUpView.as_view(), name="doctor_signup"),
     path("auth/verify/", VerifyCodeView.as_view(), name="verify_code"),
+    
+    # local apps
     path("user/", include("user.urls", namespace="user")),
-
     path("", include("reservation.urls", namespace="reservation")),
     path("rating/", include("rating.urls", namespace="rating")),
     path("comment/", include("comment.urls", namespace="comment")),
